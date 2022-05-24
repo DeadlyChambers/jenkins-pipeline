99ee48570462a34ccc7554bd767f068051da9dab
DeadlyChambers <shanechambers85@gmail.com>
2022-05-24 08:44:50 -0600

    updated jenkinsfiles

--- new/jenkinsfile.groovy
+++ new/jenkinsfile.groovy
@@ -11,15 +11,25 @@ pipeline {
         //'https://DeadlyChambers85@bitbucket.org/DeadlyChambers85/practice.git'
         deployBranch = 'main'
         APP_VER = "6.0.${BUILD_NUMBER}"
+        APP_LABEL = ""
     }
     stages {
+        
         stage('Preparation') { // for display purposes
             steps {
-                sshagent(credentials: [gitCredentialId]) {
-                    git branch: deployBranch, url: gitUrl
+                
+                sshagent (credentials: ['bb-ssh']) {
+                    
+                    sh("git clone ${env.gitUrl}")
+                    script{
+                        APP_VER = sh(returnStdout: true, script:'git tag | sort -V | tail -1 | cut -d "v" -f2').trim()
+                    }
+                }
+                script{
+                    env.APP_LABEL="system${APP_VER}-6.0.${env.BUILD_NUMBER}"
                 }
                 sh 'ls -la'
-                echo 'here do a shallow git to keep size down'
+                echo 'here do a shallow git to keep size down - ${env.APP_LABEL}'
                 echo 'if you can cache the repo that could speed up build time'
             }
         }
@@ -27,27 +37,22 @@ pipeline {
             steps {
                 sh '''
                 dotnet build api/system/system.csproj -c Release -r \
-                linux-x64 -p:Version="$APP_VER"
+                linux-x64 -p:Version="6.0.${env.BUILD_NUMBER}"
                 dotnet publish api/system/system.csproj -c Release -r \
-                linux-x64 -o ../out --self-contained -p:Version="$APP_VER"
+                linux-x64 -o ../out --self-contained -p:Version="6.0.${env.BUILD_NUMBER}"
+                zip -r ${env.APP_LABEL}.zip . -i out
+                echo "build, run tests"
                 '''
-                // dotnetBuild(project:'api/system/system.csproj', configuration:'Release',
-                //     runtime:'linux-x64', propertiesString: "Version=$APP_VER")
-                // dotnetPublish(project:'api/system/system.csproj',
-                //     configuration:'Release', runtime:'linux-x64', selfContained:true,
-                //     outputDirectory: '../out', propertiesString: "Version=$APP_VER")
-                sh "zip -r system${APP_VER}.zip . -i out"
-                echo 'build, run tests'
-                s3Upload(file:"system${APP_VER}.zip",
-                bucket: "soinshane-terraform-state")
+                //s3Upload(file:"system${env.APP_VER}.zip",
+                //bucket: "soinshane-terraform-state")
             }
         }
         stage('Create Release') {
                 steps {
                     sh 'which aws'
                     echo 'create the zip, and push to beanstalk'
-                    echo "s3_file=s3://bucket/app_version/${env.BUILD_NUMBER}.zip"
-                    build job: 'create-release', parameters: [string(name: 'Label', value: "${env.BUILD_NUMBER}")]
+                    echo "s3_file=s3://bucket/app_version/${env.APP_LABEL}.zip"
+                    build job: 'dotnet-deploy-pipeline', parameters: [string(name: 'Label', value: "${env.APP_LABEL}"), string(name: 'ENV', value: "")]
                 }
         }
 
@@ -56,7 +61,7 @@ pipeline {
                 sh 'which curl'
                 echo 'push the file to s3, kick off the beanstalk create app version'
                 echo 'Then we should have a prepared deploy for all environments'
-                echo "archiveArtifacts ${env.BUILD_NUMBER}"
+                echo "archiveArtifacts ${env.APP_LABEL}"
             }
         }
     }
@@ -65,7 +70,7 @@ pipeline {
             echo '========always========'
         }
         success {
-            cleanWs()
+            //cleanWs()
             echo '========pipeline executed successfully ========'
         }
         failure {
--- remote-pipelines/jenkinsfile.groovy
+++ remote-pipelines/jenkinsfile.groovy
@@ -1,45 +1,59 @@
 
 pipeline {
     agent none
+    parameters{
+        extendedChoice(
+            name: 'ENV',
+            defaultValue: "none",
+            multiSelectDelimiter: ',',
+            type: 'PT_CHECKBOX',
+            value: 'none,qa,stage,live'
+        )
+    }
     options {
         skipDefaultCheckout()
         disableConcurrentBuilds()
     }
-    parameters {
-        choice(name: 'ENV', default: '', choices: ['', 'qa', 'stage', 'prod'], description: 'Run for environment')
-    }
     stages {
         stage('Init') {
             when { triggeredBy 'BuildUpstreamCause' }
-        }
-        stage('Deploy') {
-            when {
-                expression { params.ENV == env.deployENV }
+            steps {
+                script {
+                    def upstream = currentBuild.rawBuild.getCause(hudson.model.Cause$UpstreamCause)
+                    currentBuild.displayName = string(upstream.getShortDescription()+'.'+upstream.getUpstreamBuild())
+                    currentBuild.description = upstream.getUpstreamRun().getDisplayName()
+                }
             }
-            matrix {
-                axes {
-                    axis {
-                        name 'deployENV'
-                        values 'qa', 'stage', 'prod'
-                    }
-                }
-
-                stages {
-                    when {
-                        ENV: deployEnv
-                    }
-                    stage('DeployS3') {
-                        steps {
-                            echo "Do Build for ${PLATFORM} - ${BROWSER}"
-                        }
-                    }
-                    stage('Test') {
-                        steps {
-                            echo "Do Test for ${PLATFORM} - ${BROWSER}"
-                        }
-                    }
-                }
+        }
+        stage('Deploy QA') {
+            when {
+                expression { params.ENV == 'qa' }
+            }
+            steps{
+                echo "Do Build for ${params.ENV} - ${BROWSER}"
+            }
+        }
+        stage('Deploy Stage') {
+            when {
+                expression { params.ENV == 'stage' }
+            }
+            steps {
+                echo "Do Test for ${PLATFORM} - ${BROWSER}"
+            }
+        }
+        stage('Deploy Live') {
+            when {
+                expression { params.ENV == 'live'}
+            }
+            steps {
+                echo "Do Test for ${PLATFORM} - ${BROWSER}"
             }
         }
     }
+    post {
+        always {
+            echo "=====${currentBuild.displayName}=====${currentBuild.description}======"
+        }
+    }
 }
+
